16e3392163ddc86808d3f8cd56ff43a5910b24e2
Hebert Alves Ferreira <c927553@pmmg.mg.gov.br>
2024-05-06 17:50:21 -0300

    theme saved on localstorage

--- src/app/components/toggle-theme/toggle-theme.component.ts
+++ src/app/components/toggle-theme/toggle-theme.component.ts
@@ -1,21 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'neo-toggle-theme',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './toggle-theme.component.html',
-  styleUrl: './toggle-theme.component.css'
+  styleUrl: './toggle-theme.component.css',
 })
-export class ToggleThemeComponent {
-
+export class ToggleThemeComponent implements OnInit {
   darkTheme = false;
 
-  toggleTheme(){
-    this.darkTheme = !this.darkTheme;
+  constructor() {}
 
+  ngOnInit(): void {
+    this.loadTheme();
+  }
+
+  loadTheme(): void {
+    const themeStorage = localStorage.getItem('theme');
+    this.darkTheme = themeStorage === 'dark';
+    this.applyTheme();
+  }
+
+  applyTheme(): void {
     const theme = this.darkTheme ? 'dark-theme' : '';
-    document.body.className = theme;
+    this.setBodyClassName(theme);
+  }
+
+  toggleTheme(): void {
+    this.darkTheme = !this.darkTheme;
+    this.applyTheme();
+    localStorage.setItem('theme', this.darkTheme ? 'dark' : 'light');
+  }
+
+  setBodyClassName(className: string): void {
+    document.body.className = className;
   }
 }
+
+
